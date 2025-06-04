43748ca745bfe23112157aab90409f3d2d481408
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-04 13:40:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ static/sw.js
@@ -0,0 +1,66 @@
+// Service Worker for EasyMoney
+
+const CACHE_NAME = 'easymoney-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css.css',
+  '/js.js',
+  '/manifest.json',
+  '/favicon.ico'
+];
+
+// Install event - cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached response if found
+        if (response) {
+          return response;
+        }
+        
+        // Otherwise fetch from network
+        return fetch(event.request)
+          .then(response => {
+            // Don't cache non-successful responses
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Clone the response
+            const responseToCache = response.clone();
+            
+            // Add to cache
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+      })
+  );
+});
